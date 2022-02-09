e390392758f8764bed506d783096b65e807bcefc
Ryan <ryan.oliver.j12@gmail.com>
2022-02-08 19:04:22 -0800

    task 3

--- index.js
+++ index.js
@@ -115,8 +115,29 @@ Puppies less than 1 year
 NOTE: If done correctly, a weight of 15 lbs and age of 1 year would return 0.44999999999999996
 */  
 
-function hungryDog(/*add your code here*/){
-  /*add your code here*/
+function hungryDog(weight, age){
+  if (age >= 1) {
+    if (weight <= 5) {
+      return (weight * .05);
+    } else if (weight > 5 && weight <= 10) {
+      return (weight * .04);
+    } else if (weight > 10 && weight <= 15) {
+      return (weight * .03);
+    } else if (weight > 15) {
+      return (weight * .02);
+    } else {
+      return 'something went wrong'
+    }
+  } else if (age < 1) {
+    if (age >= .167 && age < .333) {
+      return (weight * .1);
+    } else if (age > .333 && age <= .583) {
+      return (weight * .05);
+    } else if (age > .583) {
+      return (weight * .04);
+    } else {
+      return 'something went wrong'
+  }
 }
 
 
@@ -253,4 +274,5 @@ module.exports = {
   feet,
   annoyingSong,
   grade
+}
 }
